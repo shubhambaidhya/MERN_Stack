6f3783618014d391e896e011b6b76a6b263eb577
Shubham Man Singh Baidhya <shubhambaidhya7@gmail.com>
2024-06-15 08:32:00 +0545

    Add files via upload

---
+++ z6.asynchronous.js
@@ -0,0 +1,28 @@
+//? asynchronous
+
+//? synchronous => existing or occurring at same/similar time
+//? asynchronous => not existing or occurring at same/similar time
+
+//? node => single threaded
+//? non-blocking => does not block code execution
+//? real world scenario of code blocking =>
+//? for loop billion times
+//? image processing
+//? API request
+//? database operation
+
+//? interview ma asked questions
+//? kasari print huncha check it....
+// console.log("Hi");
+
+// setTimeout(() => {
+//   console.log("TimeOut"), 2000;
+// });
+// console.log("Hello");
+
+// u => ultra power saving
+// k => unlocked
+// H => high performance
+// H => Hexa core
+
+// const multiply = (x, y) => x * y;
